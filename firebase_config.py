7174bbcdcdc9bbbfb9d4554ab9479819aaea3dbf
koryroot <20197914@itla.edu.do>
2025-08-08 11:15:59 -0400

    configuracion firebase

---
+++ firebase_config.py
@@ -0,0 +1,10 @@
+# firebase_config.py
+config = {
+    "apiKey": "AIzaSyC_RbyMkEhwK3T5_Hpl05mJnQQYCuunBj4",
+    "authDomain": "billetera-inteligente-app.firebaseapp.com",
+    "projectId": "billetera-inteligente-app",
+    "storageBucket": "billetera-inteligente-app.firebasestorage.app",
+    "messagingSenderId": "818140911365",
+    "appId": "1:818140911365:web:1f620869505658fa68896c",
+    "databaseURL": ""
+}
